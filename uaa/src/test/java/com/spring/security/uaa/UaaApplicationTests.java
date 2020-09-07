45b45faaad7588d102d95e6a459c248cd3a552b5
luyanan <luyanan0718@163.com>
2020-09-07 21:58:09 +0800

    batch comit

--- uaa/src/test/java/com/spring/security/uaa/UaaApplicationTests.java
+++ uaa/src/test/java/com/spring/security/uaa/UaaApplicationTests.java
@@ -1,12 +1,15 @@
 package com.spring.security.uaa;
 
+import cn.hutool.core.io.FileUtil;
+import org.jsoup.Jsoup;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UaaApplicationTests {
 
-    void contextLoads() {
+    public static void main(String[] args) {
+        String file = FileUtil.readString("D:/工程.txt", "utf-8");
+        System.out.println(file);
+        System.out.println(Jsoup.parse(file).text());
     }
-
 }
